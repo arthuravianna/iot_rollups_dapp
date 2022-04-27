2542bccb449e23183772e645e5e8cbe3bd0d9103
ArtV97 <arthurvianna@id.uff.br>
2022-04-27 15:19:27 -0300

    pagination title fix

--- front_end/controllers/home.js
+++ front_end/controllers/home.js
@@ -32,46 +32,46 @@ module.exports={
                 }
 
                 if (last_page <= 3) {
-                    pagination.push( {"label": "prev", "val": page-1, "title": "Previous Epoch", "disabled": prev_disabled} )
+                    pagination.push( {"label": "prev", "val": page-1, "title": "Previous-Epoch", "disabled": prev_disabled} )
                     
                     for (var i = 1; i <= last_page; i++) {
                         if (i == page) {
-                            pagination.push( {"label": page, "val": page, "title": `Epoch ${req_epoch}`, "disabled": true} )
+                            pagination.push( {"label": page, "val": page, "title": `Epoch-${req_epoch}`, "disabled": true} )
                         }
                         else {
-                            pagination.push( {"label": i, "val": i, "title": `Epoch ${i-1}`, "disabled": false} )
+                            pagination.push( {"label": i, "val": i, "title": `Epoch-${i-1}`, "disabled": false} )
                         }   
                     }
                     
-                    pagination.push( {"label": "next", "val": page+1, "title": "Next Epoch", "disabled": next_disabled} )
+                    pagination.push( {"label": "next", "val": page+1, "title": "Next-Epoch", "disabled": next_disabled} )
                 }
                 else {
-                    pagination.push( {"label": "prev", "val": page-1, "title": "Previous Epoch", "disabled": prev_disabled} )
+                    pagination.push( {"label": "prev", "val": page-1, "title": "Previous-Epoch", "disabled": prev_disabled} )
                     
                     
                     pagination.push( {"label": 1, "val": 1, "title": "Epoch 0", "disabled": false} )
                     if (page - 1 == 2) {
-                        pagination.push( {"label": page-1, "val": page-1, "title": `Epoch ${req_epoch-1}`, "disabled": false} )
+                        pagination.push( {"label": page-1, "val": page-1, "title": `Epoch-${req_epoch-1}`, "disabled": false} )
                     }
                     else {
                         pagination.push( {"label": "...", "val": "", "title": "", "disabled": true} )
-                        pagination.push( {"label": page-1, "val": page-1, "title": `Epoch ${req_epoch-1}`, "disabled": false} )
+                        pagination.push( {"label": page-1, "val": page-1, "title": `Epoch-${req_epoch-1}`, "disabled": false} )
                     }
                     
                     
-                    pagination.push( {"label": page, "val": page, "title": `Epoch ${req_epoch}`, "disabled": true} )                                   
+                    pagination.push( {"label": page, "val": page, "title": `Epoch-${req_epoch}`, "disabled": true} )                                   
                     
                     
-                    pagination.push( {"label": page+1, "val": page+1, "title": `Epoch ${req_epoch+1}`, "disabled": true} )
+                    pagination.push( {"label": page+1, "val": page+1, "title": `Epoch-${req_epoch+1}`, "disabled": true} )
                     if (page + 1 == last_page - 1) {
-                        pagination.push( {"label": last_page, "val": last_page, "title": `Epoch ${current_epoch}`, "disabled": false} )
+                        pagination.push( {"label": last_page, "val": last_page, "title": `Epoch-${current_epoch}`, "disabled": false} )
                     }
                     else {
                         pagination.push( {"label": "...", "val": "", "title": "", "disabled": true} ) 
-                        pagination.push( {"label": last_page, "val": last_page, "title": `Epoch ${current_epoch}`, "disabled": false} )
+                        pagination.push( {"label": last_page, "val": last_page, "title": `Epoch-${current_epoch}`, "disabled": false} )
                     }
                     
-                    pagination.push( {"label": "next", "val": page+1, "title": "Next Epoch", "disabled": next_disabled} )
+                    pagination.push( {"label": "next", "val": page+1, "title": "Next-Epoch", "disabled": next_disabled} )
                 }
             }
 
