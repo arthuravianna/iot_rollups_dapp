a21f24bc6b36537aa459876865cc7c5837fa5d7d
ArtV97 <arthurvianna@id.uff.br>
2022-05-23 17:31:07 -0300

    functional v0.2

--- front_end/config/eth-connection.js
+++ front_end/config/eth-connection.js
@@ -6,41 +6,8 @@ const web3 = new Web3(Web3.givenProvider || provider)
 
 web3.eth.getAccounts().then(console.log).catch(function(error) {console.log("Error: Couldn't get accounts list!")});
 
-
-// const rollups_contract_addr = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853"
-// const rollups_jsonInterface = require("../public/ABI/RollupsImpl_ABI.json")
-// const rollups_contract = new web3.eth.Contract(rollups_jsonInterface, rollups_contract_addr)
-
-// const input_jsonInterface = require("../public/ABI/InputImpl_ABI.json")
-// async function load_input_contract() {
-//     return rollups_contract.methods.getInputAddress().call({ from: "0xcd3b766ccdd6ae721141f452c550ca635964ce71" })
-//         .then(function (result) { // result is Input contract address
-//             const input_contract_addr = result
-    
-//             console.log("Input Contract Loaded!")
-//             return new web3.eth.Contract(input_jsonInterface, input_contract_addr) // return input contract
-//         })
-//         .catch(function(error) {
-//             console.log("# load input contract error \n", error, "#\n")
-//             return error
-//         })
-// }
-
-// module.exports = (async function() {
-//         const input_contract = await load_input_contract()
-//         return { web3: web3, rollups_contract: rollups_contract, input_contract: input_contract }
-//     })();
-
-// const rollups_facet = require("../public/ABI/localhost/RollupsFacet.json")
-// const input_facet = require("../public/ABI/localhost/InputFacet.json")
-
-// const rollups_contract = new web3.eth.Contract(rollups_facet.abi, rollups_facet.address)
-// const input_contract = new web3.eth.Contract(input_facet.abi, input_facet.address)
-
 const hardhat = require("../public/ABI/localhost.json")
-
-
-const rollups_contract = new web3.eth.Contract(hardhat.contracts.RollupsFacet.abi, hardhat.contracts.RollupsFacet.address)
-const input_contract = new web3.eth.Contract(hardhat.contracts.InputFacet.abi, hardhat.contracts.InputFacet.address)
+const rollups_contract = new web3.eth.Contract(hardhat.contracts.RollupsFacet.abi, hardhat.contracts.CartesiDApp.address)
+const input_contract = new web3.eth.Contract(hardhat.contracts.InputFacet.abi, hardhat.contracts.CartesiDApp.address)
 
 module.exports = { web3: web3, rollups_contract: rollups_contract, input_contract: input_contract }
--- front_end/test.js
+++ front_end/test.js
@@ -5,15 +5,15 @@ const web3 = new Web3(Web3.givenProvider || provider);
 
 
 const hardhat = require("./public/ABI/localhost.json")
-const rollups_contract = new web3.eth.Contract(hardhat.contracts.RollupsFacet.abi, hardhat.contracts.RollupsFacet.address)
-const input_contract = new web3.eth.Contract(hardhat.contracts.InputFacet.abi, hardhat.contracts.InputFacet.address)
+const rollups_contract = new web3.eth.Contract(hardhat.contracts.RollupsFacet.abi, hardhat.contracts.CartesiDApp.address)
+const input_contract = new web3.eth.Contract(hardhat.contracts.InputFacet.abi, hardhat.contracts.CartesiDApp.address)
 
-//web3.eth.defaultAccount = ... // to set a default "from" parameter
+// web3.eth.defaultAccount = ... // to set a default "from" parameter
 web3.eth.getAccounts()
 .then(function(result) {
     accounts = result
 
-    console.log(accounts[0])
+    console.log("Account:", accounts[0])
     rollups_contract.methods.getCurrentEpoch().send({ from: accounts[0] })
     .then(function(result) {
         console.log("Current Epoch:", result)
@@ -23,23 +23,22 @@ web3.eth.getAccounts()
     })
 
     // Echo DApp input
-    //const input_hex = "0x63617274657369"
+    // const input_hex = "0x63617274657369"
 
     // my DApp input
     const input_obj = { "bus_id":  "18C", "trip_id": "18C;1", "lat": 57.82847892, "lon": 26.53362055, "ts": "2022-03-25 12:46:30" }
     const input_hex = web3.utils.utf8ToHex(JSON.stringify(input_obj))
-    console.log(input_hex)
-    
+    console.log("Input Hex:", input_hex)
+
     input_contract.methods.addInput(input_hex).send({ from: accounts[0] })
     .then(function(result) {
         console.log("addInput:", result)
     })
     .catch(function(error) {
-        console.log(error)
+        console.log("AddInput", error)
     })
     
 })
 .catch(function(error) {
     console.log("Error: Couldn't get accounts list!")
 });
-
